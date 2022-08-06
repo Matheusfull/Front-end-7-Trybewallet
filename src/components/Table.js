7cf1b8a0f44cc877e67187bd6eb5cc8412a3d9f8
Matheusfull <fpmitmatheus@yahoo.com>
2022-08-06 17:05:55 -0500

    requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,80 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    // const { description, tag, method, currency, value } = expenses;
+    // const { exchangeRates.currency.name } = expenses;
+    console.log(expenses);
     return (
-      <div>
+      <table>
         <tr>
-          <th>Descrição:</th>
-          <th>Tag:</th>
-          <th>Método de pagamento:</th>
-          <th>Valor:</th>
-          <th>Moeda:</th>
-          <th>Câmbio utilizado:</th>
-          <th>Valor convertido:</th>
-          <th>Moeda de conversão:</th>
-          <th>Editar/Excluir:</th>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
         </tr>
-      </div>
+        <tbody>
+          {
+            expenses.map((expense, index) => (
+              <tr key={ index }>
+                <td>
+                  {/* Descrição: */}
+                  { expense.description }
+                </td>
+                <td>
+                  {/* Tag: */}
+                  { expense.tag }
+                </td>
+                <td>
+                  {/* Método de pagamento: */}
+                  { expense.method }
+                </td>
+                <td>
+                  {/* Valor: */}
+                  { parseFloat(expense.value).toFixed(2) }
+                </td>
+                <td>
+                  {/* Moeda: */}
+                  { expense.exchangeRates[expense.currency].name }
+                </td>
+                <td>
+                  {/* Câmbio utilizado: */}
+                  {/* { expense.currency } eu estava colocando o símbolo da moeda */}
+                  { parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2) }
+                </td>
+                <td>
+                  {/* Valor convertido: */}
+                  { ((expense.value)
+                  * parseFloat(expense.exchangeRates[expense.currency].ask)).toFixed(2) }
+                </td>
+                <td>
+                  {/* Moeda de conversão: */}
+                  Real
+                </td>
+                <td>Editar</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: propTypes.arrayOf(propTypes.object),
+}.isRequerid;
+
+export default connect(mapStateToProps, null)(Table);
